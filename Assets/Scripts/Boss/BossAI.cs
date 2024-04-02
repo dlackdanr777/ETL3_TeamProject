12e65d1c7d43b2178d899cec0ec7a59dbff27416
dlackdanr777 <64966089+dlackdanr777@users.noreply.github.com>
2024-04-02 21:30:12 +0900

    [move] 씬들 폴더 이동

--- Assets/Scripts/Boss/BossAI.cs
+++ Assets/Scripts/Boss/BossAI.cs
@@ -1,17 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>보스의 현재 상태</summary>
-public enum BossAIState
-{
-    Idle,
-    Tracking,
-    Backstep,
-    Reconnaissance,
-    Attack,
-    Guard,
-    Die,
-}
 
 
 public class BossAI
---
+++ Assets/Scripts/Boss/BossAIState.cs
@@ -0,0 +1,11 @@
+/// <summary>보스의 현재 상태</summary>
+public enum BossAIState
+{
+    Idle,
+    Tracking,
+    Backstep,
+    Reconnaissance,
+    Attack,
+    Guard,
+    Die,
+}
